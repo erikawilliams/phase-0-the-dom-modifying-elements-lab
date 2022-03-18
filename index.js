a1eff1a8c4b48bc1bce7e8a23b39e9bd9f51916f
Erika Williams <erika@erikawilliams.us>
2022-03-18 11:12:38 -0600

    commit tree 7f102d158e2a8ed75518541f077d5b67d266197d

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const main = document.querySelector("main#main")
+console.log(main)
+main.remove();
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory";
+
+
+newHeader.innerHTML = "erika is the champion";
+document.body.append(newHeader)
+console.log(newHeader);
